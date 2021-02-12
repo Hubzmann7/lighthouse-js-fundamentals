54cd33966be547ebb27162b37f9b011754a4b871
Sven Bellamy <sven.bellamy@gmail.com>
2021-02-12 22:16:47 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -15,6 +15,7 @@ if (cold) {
 console.log("Now you're ready to go outside!");
 */
 
+/*
 const temperature = -1;
 
 if (temperature < 0) {
@@ -26,4 +27,11 @@ if (temperature < 0) {
 } 
 
 console.log("Now you're ready to go outside!");
+*/
 
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
