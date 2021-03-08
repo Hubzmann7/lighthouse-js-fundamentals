5cc7a2a59e92fdc40b8837615ad74c3fd40f19a4
Sven Bellamy <sven.bellamy@gmail.com>
2021-03-08 21:13:40 +0000

    commiting even.js & odd.js

---
+++ even.js
@@ -0,0 +1,12 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+// isEven takes as its parameter a number and returns a Boolean value representing whether or not the number is even.
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
