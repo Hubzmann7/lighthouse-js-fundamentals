325c466724b0d0b8a42f0242d51d1768ab347126
Sven Bellamy <sven.bellamy@gmail.com>
2021-03-13 21:03:30 +0000

    changes to iterating over arrays

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,16 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+// Iterating over an Array with `for`
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+// Iterating over an Array with `while`
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+
