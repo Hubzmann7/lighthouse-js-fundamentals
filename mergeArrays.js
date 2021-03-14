4dc5e65066c776b77b2ec9382777dbea39de22f2
Sven Bellamy <sven.bellamy@gmail.com>
2021-03-14 04:11:40 +0000

    looping over arrays exercises

---
+++ mergeArrays.js
@@ -0,0 +1,15 @@
+function merge(arrayOne, arrayTwo) {
+  let arrayOneSorted = arrayOne.sort();
+  let arrayTwoSorted = arrayTwo.sort();
+
+  for (var i = 0; i < arrayTwoSorted.length; i++) {
+    arrayOneSorted.push(arrayTwoSorted[i]);
+  }
+
+  return arrayOneSorted.sort();
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
+
